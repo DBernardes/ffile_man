5af9e56956c6f75e2cd870234d54e667ca4bda72
DBernardes <denis.bernardes099@gmail.com>
2024-07-17 15:10:39 -0300

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,11 +16,12 @@ project = "FITS FIle Manager"
 copyright = "2024, Bernardes, D.  and Balaji, T R"
 author = "Bernardes, D.  and Balaji, T R"
 release = "v0.1.0"
+root_doc = "index"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -29,5 +30,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
---
+++ docs/source/manager.rst
@@ -0,0 +1,9 @@
+.. _manager:
+
+Manager Class
+=============
+
+This is a class that helps with the FTIS file managment.
+
+.. automodule:: manager
+   :members:
