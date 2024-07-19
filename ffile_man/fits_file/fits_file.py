6dded81171c77ef802f445abc8bc8e7e11434b1b
Balaji-0-5 <84007002+Balaji-0-5@users.noreply.github.com>
2024-07-19 13:37:26 +0000

    added feature export files to destination

--- ffile_man/fits_file/fits_file.py
+++ ffile_man/fits_file/fits_file.py
@@ -7,7 +7,15 @@ from astropy.io import fits
 
 @dataclass
 class FITS_File:
-    """Class for keeping the FITS files"""
+    """
+    Class for keeping the FITS files.
+
+    Attributes:
+        name (str): The name of the FITS file.
+        folder_path (str): The path to the folder containing the FITS file.
+        OBSTYPE (str): The observation type of the FITS file. Defaults to an empty string.
+        DATEOBS (str): The date of the observation. Defaults to an empty string.
+    """
 
     name: str
     folder_path: str
@@ -15,13 +23,28 @@ class FITS_File:
     DATEOBS: str = ""
 
     def _getheaders(self) -> None:
+        """
+        Retrieve the header information from the FITS file.
+        """
         file_path = os.path.join(self.folder_path, self.name)
         self.header = fits.getheader(file_path)
 
     def _extractheaderinfo(self) -> None:
+        """
+        Extract relevant information from the FITS file header and set the OBSTYPE and DATEOBS attributes.
+        """
         self._getheaders()
         self.OBSTYPE = self.header["OBSTYPE"]
         self.DATEOBS = datetime.fromisoformat(self.header["DATE-OBS"])
 
     def __lt__(self, other):
+        """
+        Compare FITS files based on the DATEOBS attribute.
+
+        Args:
+            other (FITS_File): The other FITS_File object to compare with.
+
+        Returns:
+            bool: True if the DATEOBS of the current file is less than the DATEOBS of the other file.
+        """
         return self.DATEOBS < other.DATEOBS
--- ffile_man/manager/manager.py
+++ ffile_man/manager/manager.py
@@ -2,16 +2,31 @@ import os
 
 from ..fits_file import FITS_File
 
+import shutil
+
 
 class Manager:
 
     def __init__(self, images_folder, sub_folders=False) -> None:
+        """
+        Initialize the Manager with the specified folder containing FITS files.
+
+        Args:
+            images_folder (str): Path to the folder containing FITS files.
+            sub_folders (bool, optional): Whether to include files from subfolders. Defaults to False.
+        """
         self.images_folder = images_folder
         self.sub_folders = sub_folders
         self.read_folder()
         return
 
     def read_folder(self) -> list:
+        """
+        Read the specified folder and subfolders to find FITS files and extract their header information.
+
+        Returns:
+            list: A list of FITS_File objects found in the folder.
+        """
         self.fits_files = []
         self.fits_files += [
             FITS_File(file, folder_path=self.images_folder)
@@ -46,10 +61,21 @@ class Manager:
         self.fits_files = [file for file in self.fits_files if file.OBSTYPE == obstype]
 
     def print_list(self) -> str:
-        """print the list of files"""
+        """
+        Print the list of FITS files.
+
+        Returns:
+            str: Formatted string of FITS files.
+        """
         print(*self.fits_files, sep="\n")
 
     def __lt__(self, other):
+        """
+        Filter the FITS files to include only those with DATEOBS less than the given date.
+
+        Args:
+            other (str): Date to compare with.
+        """
         filtered_files = []
         for file in self.fits_files:
             if file.DATEOBS < other:
@@ -57,6 +83,12 @@ class Manager:
         self.fits_files = filtered_files
 
     def __gt__(self, other):
+        """
+        Filter the FITS files to include only those with DATEOBS greater than the given date.
+
+        Args:
+            other (str): Date to compare with.
+        """
         filtered_files = []
         for file in self.fits_files:
             if file.DATEOBS > other:
@@ -64,6 +96,12 @@ class Manager:
         self.fits_files = filtered_files
 
     def __le__(self, other):
+        """
+        Filter the FITS files to include only those with DATEOBS less than or equal to the given date.
+
+        Args:
+            other (str): Date to compare with.
+        """
         filtered_files = []
         for file in self.fits_files:
             if file.DATEOBS <= other:
@@ -71,6 +109,12 @@ class Manager:
         self.fits_files = filtered_files
 
     def __ge__(self, other):
+        """
+        Filter the FITS files to include only those with DATEOBS greater than or equal to the given date.
+
+        Args:
+            other (str): Date to compare with.
+        """
         filtered_files = []
         for file in self.fits_files:
             if file.DATEOBS >= other:
@@ -78,6 +122,15 @@ class Manager:
         self.fits_files = filtered_files
 
     def obs_type(self, obs_types):
+        """
+        Filter the FITS files based on the observation type(s).
+
+        Args:
+            obs_types (str or list): Observation type(s) to filter by. Can be a string or a list of strings.
+
+        Raises:
+            TypeError: If obs_types is not a string or a list of strings.
+        """
         if isinstance(obs_types, str):
             filtered_list = []
             for file in self.fits_files:
@@ -94,3 +147,17 @@ class Manager:
             self.fits_files = filtered_list
         else:
             raise TypeError("Input must be of type string or list(of strings)")
+
+    def export_to(self,destination_folder,move=False):
+        """
+        Export FITS files to a specified destination folder.
+
+        Args:
+            destination_folder (str): The path to the folder where the FITS files will be exported.
+            move (bool, optional): If True, move the files instead of copying them. Defaults to False.
+        """
+        for file in self.fits_files:
+            if move:
+                shutil.move(os.path.join(file.folder_path,file.name),os.path.join(destination_folder)))
+            else:
+                shutil.copy(os.path.join(file.folder_path,file.name),os.path.join(destination_folder)))
